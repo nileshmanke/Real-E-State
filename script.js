73e1e40e4ce95f39dadea6a5143807c64956edee
Nilesh Ravindra Manke <nileshmanke12ab@gmail.com>
2025-03-31 01:35:41 +0530

    add Email facility

--- script.js
+++ script.js
@@ -14,3 +14,28 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+(function(){
+    emailjs.init("O9IFE8zq5fjh3gYYw"); // Replace with your EmailJS Public Key
+})();
+
+function sendEmail(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Get form values
+    let formData = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+    };
+
+    // Send Email via EmailJS
+    emailjs.send("service_b810lei", "template_ywcyiyj", formData)
+    .then(function(response) {
+        alert("Message Sent Successfully! ✅");
+        document.getElementById("contact-form").reset(); // Clear form
+    }, function(error) {
+        alert("Failed to Send Message ❌, Try Again!");
+    });
+}
